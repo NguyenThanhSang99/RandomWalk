3751855210f120f452fce22215c4316d7b45d9d5
NguyenThanhSang99 <50515431+NguyenThanhSang99@users.noreply.github.com>
2022-03-05 17:17:49 +0700

    Apply random walk algorithm

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# RandomWalk
+# Random Walk Algorithm
+
 This is an example of the Random Walk algorithm
+
+## Random Walk 1D
+
+![Image of random walk 1D](https://github.com/NguyenThanhSang99/RandomWalk/blob/main/random_walk_1D.png?raw=true)
+
+## Random Walk 2D
+
+![Image of random walk 2D](https://github.com/NguyenThanhSang99/RandomWalk/blob/main/random_walk_2D.png?raw=true)
---
+++ randomWalk1D.py
@@ -0,0 +1,25 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+# The probability to move step
+probability=[0.04, 0.99]
+
+# Start point and the position
+start=1
+position=[start]
+
+# creating ramdom point
+rr = np.random.random(10000)
+down_point = rr < probability[0]
+up_point = rr > probability[1]
+
+for i_down, i_up in zip(down_point, up_point):
+    down = i_down and position[-1] > 1
+    up = i_up and position[-1] < 10
+    position.append(position[-1] - down + up)
+
+# plotting down the graph of the random walk in 1D
+plt.plot(position)
+plt.show()
+plt.savefig("rand_walk_1D.png",bbox_inches="tight",dpi=600)
